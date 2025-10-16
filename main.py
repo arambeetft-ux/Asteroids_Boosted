1853cc8016e4a347b2afa84e34bb754f740f5d57
Arambee <arambeetft@gmail.com>
2025-10-16 21:49:10 +0200

    End of Boot.dev tutorial for the project

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+from shot import Shot
 
 
 
@@ -12,17 +15,59 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)   
+
+    All_asteroids = pygame.sprite.Group()
+    Asteroid.containers = (All_asteroids, updatable, drawable)
+    
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
+
+    All_shots = pygame.sprite.Group()
+    Shot.containers = (All_shots, updatable, drawable)
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+
+        for asteroid in All_asteroids:
+            if player.Colision_Check(asteroid):
+                print("Collision detected!")
+                All_asteroids.remove(asteroid)
+                drawable.remove(asteroid)
+                updatable.remove(asteroid)
+                print("Game Over!")
+                return
+
+        for asteroid in All_asteroids:
+            for shot in All_shots:
+                if shot.Colision_Check(asteroid):
+                    print("Shot hit asteroid!")
+                    All_asteroids.remove(asteroid)
+                    drawable.remove(asteroid)
+                    updatable.remove(asteroid)
+                    All_shots.remove(shot)
+                    drawable.remove(shot)
+                    updatable.remove(shot)
+                    if asteroid.radius > ASTEROID_MIN_RADIUS:
+                        for _ in range(2):
+                            new_asteroid = Asteroid(asteroid.position.x, asteroid.position.y, asteroid.radius - ASTEROID_MIN_RADIUS)
+                            new_asteroid.velocity = asteroid.velocity.rotate(30 * (_ * 2 - 1))
+                    break
+
+        for drawables in drawable:
+            drawables.draw(screen)
         pygame.display.flip()        
         clock.tick(60)        
-        dt += clock.get_time() / 1000.0  # Convert milliseconds to seconds
+        dt = clock.tick(60) / 1000.0  # Convert milliseconds to seconds
     
 
 
